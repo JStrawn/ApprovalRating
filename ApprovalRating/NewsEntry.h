a4513d30c2355c69ee7561f1b0ec240676093932
Louis <Lharris134@gmail.com>
2017-03-02 15:10:31 -0500

    Added News EntryVC to get articles.

---
+++ ApprovalRating/NewsEntry.h
@@ -0,0 +1,20 @@
+//
+//  NewsEntry.h
+//  ApprovalRating
+//
+//  Created by Louis Harris on 3/2/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+@interface NewsEntry : UIViewController<WKNavigationDelegate>
+
+//(might not be needed) @property (strong, nonatomic)NSString* newsTitle;
+@property (retain, nonatomic)NSURL* newsArticleURL;
+@property (strong, nonatomic)WKWebView* webView;
+@property (strong, nonatomic)NSMutableDictionary* score;
+
+
+@end
---
+++ ApprovalRating/NewsEntry.m
@@ -0,0 +1,58 @@
+//
+//  NewsEntry.m
+//  ApprovalRating
+//
+//  Created by Louis Harris on 3/2/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+#import "NewsEntry.h"
+
+@interface NewsEntry()
+
+@end
+
+@implementation NewsEntry
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self configWebView];
+    
+}
+
+-(void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    //sort through dictionary score get the hightest
+    //take the second highest or another score that's equal
+}
+
+-(void)configWebView
+{
+    WKWebViewConfiguration* theConfiguration = [[WKWebViewConfiguration alloc]init];
+    self.webView = [[WKWebView alloc]initWithFrame:self.view.frame configuration:theConfiguration];
+    self.webView.navigationDelegate = self;
+}
+
+-(void)mostPopularArticle
+{
+    //if score is the greatest number
+    NSURLRequest* nsRequest = [NSURLRequest requestWithURL:self.newsArticleURL];
+    [self.webView loadRequest:nsRequest];
+    [self.view addSubview:self.webView];
+}
+
+-(void)secondMostPopularArticle
+{
+    //if score second highest or eqaul to greatest
+    NSURLRequest* nsRequest = [NSURLRequest requestWithURL:self.newsArticleURL];
+    [self.webView loadRequest:nsRequest];
+    [self.view addSubview:self.webView];
+}
+
+@end
