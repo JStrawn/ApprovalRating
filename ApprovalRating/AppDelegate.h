16f9a9df5896091e097650b25b0c6234372c1de9
Juliana Strawn <julianastrawn@gmail.com>
2017-03-02 12:54:22 -0500

    Initial Commit

---
+++ ApprovalRating/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApprovalRating
+//
+//  Created by Juliana Strawn on 3/2/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApprovalRating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApprovalRating
+//
+//  Created by Juliana Strawn on 3/2/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
