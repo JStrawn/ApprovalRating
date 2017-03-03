4d9f0a91ecb286b8cfc0e098180790d5704d0233
Juliana Strawn <julianastrawn@gmail.com>
2017-03-02 23:37:01 -0500

    minor change

--- ViewController.h
+++ ViewController.h
@@ -20,7 +20,7 @@ alpha:1.0]
 
 @interface ViewController : UIViewController
 
-//@property (weak, nonatomic) DAO *dao;
+@property (weak, nonatomic) DAO *dao;
 
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UIButton *submitButton;
